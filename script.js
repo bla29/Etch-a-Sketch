7cb885167ac064bdad1b3945552033b1859d5369
Brian La <133820463+bla29@users.noreply.github.com>
2024-12-30 20:31:32 -0800

    Initial commit. Set up file structure

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+Create Div element that resembles a square. 
+Add a black outline to this element
+
+Add it to the container div
+
+*/
+
+const container = document.getElementById("container");
+const grid = document.createElement("div");
+grid.classList.add("grid");
+
+container.appendChild(grid);
+
+
