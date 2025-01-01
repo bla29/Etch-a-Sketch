f19d6583cdbc3a5a5d8e1a39542e4a43dac3fe72
Brian La <133820463+bla29@users.noreply.github.com>
2024-12-31 22:44:56 -0800

    input button added. grid rerender to show it.

--- script.js
+++ script.js
@@ -9,29 +9,56 @@ Add it to the container div
 const container = document.getElementById("container");
 container.classList.add("container");
 
-let counter = 1;
+const button = document.createElement("button");
+button.textContent = "# of Squares";
+document.body.prepend(button);
 
-while(counter < 257) {
+let input = 256;
+button.addEventListener('click', () => {
+    input = prompt("How many squares do you want per side?");
+    input = parseInt(input);
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    let counter = 0;
+    let gridNumber = input * input;
+    while(counter < gridNumber) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        container.appendChild(grid);
+        counter++;   
+    }
+    hover();
+});
+
+let counter = 0;
+
+while(counter < input) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
     container.appendChild(grid);
     counter++;   
 }
 
-const mouseOver = document.querySelectorAll("div.grid");
-console.log(mouseOver);
+hover();
 
-mouseOver.forEach((gridItem) => {
-    gridItem.addEventListener('mouseover', (e) => {
-        gridItem.style.backgroundColor = "red";
+function hover() {
+    const mouseOver = document.querySelectorAll("div.grid");
+    console.log(mouseOver);
+    
+    mouseOver.forEach((gridItem) => {
+        gridItem.addEventListener('mouseover', (e) => {
+            gridItem.style.backgroundColor = "red";
+        });
     });
-});
+    
+    mouseOver.forEach((gridItem) => {
+        gridItem.addEventListener('mouseleave', (e) => {
+            gridItem.style.backgroundColor = "white";
+        });
+    });
+}
 
-mouseOver.forEach((gridItem) => {
-    gridItem.addEventListener('mouseleave', (e) => {
-        gridItem.style.backgroundColor = "white";
-    });
-});
 
 
 
