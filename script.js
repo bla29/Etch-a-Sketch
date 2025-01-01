3c3e0dd68d083a7923b744b3a40450be702f667c
Brian La <133820463+bla29@users.noreply.github.com>
2024-12-31 21:56:34 -0800

    Hover effect achieved

--- script.js
+++ script.js
@@ -7,9 +7,34 @@ Add it to the container div
 */
 
 const container = document.getElementById("container");
-const grid = document.createElement("div");
-grid.classList.add("grid");
+container.classList.add("container");
 
-container.appendChild(grid);
+let counter = 1;
+
+while(counter < 257) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    container.appendChild(grid);
+    counter++;   
+}
+
+const mouseOver = document.querySelectorAll("div.grid");
+console.log(mouseOver);
+
+mouseOver.forEach((gridItem) => {
+    gridItem.addEventListener('mouseover', (e) => {
+        gridItem.style.backgroundColor = "red";
+    });
+});
+
+mouseOver.forEach((gridItem) => {
+    gridItem.addEventListener('mouseleave', (e) => {
+        gridItem.style.backgroundColor = "white";
+    });
+});
+
+
+
+
 
 
